20683c19a6da7e0acbfe9d450e3952f883bd1142
mandela.t <manderat206@gmail.com>
2022-08-11 18:13:28 +0200

    modifie the files

--- db/migrate/20220810132120_create_likes.rb
+++ db/migrate/20220810132120_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :Author_id, foreign_key: true
-      t.integer :Post_id, foreign_key: true
+      t.integer :Author_id
+      t.integer :Post_id
 
       t.timestamps
     end
--- db/migrate/20220810133159_create_comments.rb
+++ db/migrate/20220810133159_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :Author_id, foreign_key: true
-      t.integer :Post_id, foreign_key: true
+      t.integer :Author_id
+      t.integer :Post_id
       t.text :Text
 
       t.timestamps
