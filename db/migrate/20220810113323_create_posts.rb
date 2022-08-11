8389ae479bc8941c8ded5bd7663cf5d3a934ca03
mandela.t <manderat206@gmail.com>
2022-08-11 18:13:12 +0200

    Modifie the file

--- db/migrate/20220810113323_create_posts.rb
+++ db/migrate/20220810113323_create_posts.rb
@@ -1,11 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.integer :Author_id, foreign_key: true
+      t.integer :Author_id
       t.string :Title
       t.text :Text
-      t.integer :CommentsCounter
-      t.integer :LikesCounter
+      t.integer :CommentsCounter, default: 0
+      t.integer :LikesCounter, default: 0
 
       t.timestamps
     end
