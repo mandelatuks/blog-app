092ec9f8861cf9946c258698f72817f334daac0f
mandela.t <manderat206@gmail.com>
2022-08-11 18:08:00 +0200

    Add method for counting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :Author_id
-  belongs_to :Post_id
+  belongs_to :Author, class_name: 'User'
+  belongs_to :Post, class_name: 'Post'
+
+  after_save :update_comments_counter
+
+  def update_comments_counter
+    Post.increment!(:comments_counter)
+  end
 end
