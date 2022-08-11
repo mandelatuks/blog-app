14a46f2fead8d4bfea0c85c151596bfd1928bea6
mandela.t <manderat206@gmail.com>
2022-08-11 20:43:58 +0200

    Edit names

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :Author, class_name: 'User'
   has_many :likes
+  has_many :comments
 
   after_save :update_posts_counter
 
   def update_posts_counter
-    Author.increment!(:Posts_Counter)
+    Author.increment!(:posts_Counter)
   end
 
   def recent_comments
--- db/schema.rb
+++ db/schema.rb
@@ -15,47 +15,47 @@ ActiveRecord::Schema[7.0].define(version: 20_220_810_133_159) do # rubocop:disab
   enable_extension 'plpgsql'
 
   create_table 'comments', force: :cascade do |t|
-    t.integer 'Author_id'
-    t.integer 'Post_id'
-    t.text 'Text'
+    t.integer 'author_id'
+    t.integer 'post_id'
+    t.text 'text'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['Author_id'], name: 'index_comments_on_Author_id'
-    t.index ['Post_id'], name: 'index_comments_on_Post_id'
+    t.index ['author_id'], name: 'index_comments_on_author_id'
+    t.index ['post_id'], name: 'index_comments_on_post_id'
   end
 
   create_table 'likes', force: :cascade do |t|
-    t.integer 'Author_id'
-    t.integer 'Post_id'
+    t.integer 'author_id'
+    t.integer 'post_id'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['Author_id'], name: 'index_likes_on_Author_id'
-    t.index ['Post_id'], name: 'index_likes_on_Post_id'
+    t.index ['author_id'], name: 'index_likes_on_author_id'
+    t.index ['post_id'], name: 'index_likes_on_post_id'
   end
 
   create_table 'posts', force: :cascade do |t|
-    t.integer 'Author_id'
-    t.string 'Title'
-    t.text 'Text'
-    t.integer 'CommentsCounter'
-    t.integer 'LikesCounter'
+    t.integer 'author_id'
+    t.string 'title'
+    t.text 'text'
+    t.integer 'comments_counter'
+    t.integer 'likes_counter'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['Author_id'], name: 'index_posts_on_Author_id'
+    t.index ['author_id'], name: 'index_posts_on_author_id'
   end
 
   create_table 'users', force: :cascade do |t|
-    t.string 'Name'
-    t.string 'Photo'
-    t.text 'Bio'
+    t.string 'name'
+    t.string 'photo'
+    t.text 'bio'
     t.integer 'PostsCounter'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key 'comments', 'posts', column: 'Post_id'
-  add_foreign_key 'comments', 'users', column: 'Author_id'
-  add_foreign_key 'likes', 'posts', column: 'Post_id'
-  add_foreign_key 'likes', 'users', column: 'Author_id'
-  add_foreign_key 'posts', 'users', column: 'Author_id'
+  add_foreign_key 'comments', 'posts', column: 'post_id'
+  add_foreign_key 'comments', 'users', column: 'author_id'
+  add_foreign_key 'likes', 'posts', column: 'post_id'
+  add_foreign_key 'likes', 'users', column: 'author_id'
+  add_foreign_key 'posts', 'users', column: 'author_id'
 end
