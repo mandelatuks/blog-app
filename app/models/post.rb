dfc5506e888fabb1523915e5ebf74f69d1b48ea6
mandela.t <manderat206@gmail.com>
2022-08-11 18:08:52 +0200

    Add counting post method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ApplicationRecord
-  belongs_to :Author_id
+  belongs_to :Author, class_name: 'User'
+  has_many :likes
+
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    Author.increment!(:Posts_Counter)
+  end
+
+  def recent_comments
+    comments.limit(5).order(created_at: :desc)
+  end
 end
