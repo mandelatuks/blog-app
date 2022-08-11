c223d3f45e9852268f33daff4e012bbabfe4b869
mandela.t <manderat206@gmail.com>
2022-08-11 18:11:27 +0200

    Add relationship and method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  has_many :likes
+
+  # method that returns the 3 most recent posts for a given user
+  def recent_post
+    posts.limit(3).order(created_at: :desc)
+  end
 end
