5ebb07d32749577bf92bdddb1ca2618f1c3ffc75
mandela.t <manderat206@gmail.com>
2022-08-19 00:34:13 +0200

    Modifie show and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # rubocop:disable Style/SymbolArray
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+ resources :comments, only: [:new, :create]
+  resources :likes, only: [:create]
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] 
   end
 end
