1311fad6b383cbe56dfc08ae7e791d20417f5b22
mandela.t <manderat206@gmail.com>
2022-08-24 10:18:40 +0200

    Add user index integration testing

---
+++ spec/features/user_index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'user_index', type: :feature do
+  before(:each) do
+    @user = User.first
+    visit users_path
+  end
+
+  it 'shows the username of the users' do
+    expect(page).to have_content('Tom')
+  end
+
+  it 'shows the photos of the users' do
+    expect(page.html).to include('avatal')
+  end
+
+  it 'shows the number of post of each user' do
+    expect(page).to have_content("Number of Posts: #{@user.posts_counter}")
+  end
+end
