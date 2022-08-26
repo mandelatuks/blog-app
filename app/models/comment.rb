7ac3b8ec599dbe90d9a682635165fea9e15bca98
mandela.t <manderat206@gmail.com>
2022-08-26 12:24:36 +0200

    Add models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,13 @@ class Comment < ApplicationRecord
 
   after_save :update_comments_counter
 
+  after_destroy :decrease_post_comment_counter
+
   def update_comments_counter
     post.increment!(:comments_counter)
+  end
+
+  def decrease_post_comment_counter
+    post.decrement!(:comments_counter) unless post.comments_counter.zero?
   end
 end
