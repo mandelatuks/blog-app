32752dde5b9e08d62d5ffc80713bed42652173c8
mandela.t <manderat206@gmail.com>
2022-08-27 14:17:44 +0200

    Modifie user conterloller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  validates :name, presence: true
+  # validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   ROLES = %i[admin default].freeze
